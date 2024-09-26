a7a4e553922834b040b31b6b7acc2d4c1a3379b4
AprajitaChhawi <achhawip@gmail.com>
2024-09-26 22:37:23 +0530

    changes

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,12 +3,14 @@ package io.javabrains.moviecatalogservice.resources;
 import io.javabrains.moviecatalogservice.models.CatalogItem;
 import io.javabrains.moviecatalogservice.models.Movie;
 import io.javabrains.moviecatalogservice.models.Rating;
+import io.javabrains.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -35,14 +37,34 @@ public class MovieCatalogResource {
         //2.for each movie id , call movieInfo service and get details
         //3.put it all together
 
-        //1.hence this is hardcoded rating part (User has watched and rated these movies -> returned by ratings ms)
+        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/user/"+ userId , UserRating.class);
+        //2 . implementation of 2nd & 3rd part
+        return ratings.getUserRatings().stream().map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(), "Hello , this is test", rating.getRating());
+                })
+                .collect(Collectors.toList());
+
+    }
+
+    @Autowired
+    private  WebClient.Builder webClientBuilder;
+
+    @RequestMapping("/v2/{userId}")
+    public List<CatalogItem> getCatalogUsingWebClient(@PathVariable("userId") String userId){
+
         List<Rating> ratings = Arrays.asList(
                 new Rating("1234",9),
                 new Rating("1211",3)
         );
         //2 . implementation of 2nd & 3rd part
         return ratings.stream().map(rating -> {
-                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    Movie movie = webClientBuilder.build()
+                            .get()       //whatever the api type is
+                            .uri("http://localhost:8082/movies/" + rating.getMovieId()) //url of ms
+                            .retrieve()//fetch
+                            .bodyToMono(Movie.class)//whatever body you get back , covert it into an instance of movie class
+                            .block();  //we are waiting for this mono to return as return type of method is a list of catlogitems
                     return new CatalogItem(movie.getName(), "Hello , this is test", rating.getRating());
                 })
                 .collect(Collectors.toList());
--- ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/resources/RatingResource.java
+++ ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/resources/RatingResource.java
@@ -1,8 +1,13 @@
 package io.javabrains.ratingsdataservice.resources;
 import io.javabrains.ratingsdataservice.models.Rating;
+import io.javabrains.ratingsdataservice.models.UserRating;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -11,6 +16,17 @@ public class RatingResource {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId){
         return new Rating(movieId,4);
+    }
+
+    @GetMapping("/user/{userId}")
+    public UserRating getRatingForUser(@PathVariable("userId") String userId){
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234",4),
+                new Rating("1211",5)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setUserRatings(ratings);
+        return userRating;
     }
 
 }
