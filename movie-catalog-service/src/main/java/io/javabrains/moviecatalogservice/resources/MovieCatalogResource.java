038c0061e5a0cdacbe49f5533c24772408df41cd
AprajitaChhawi <achhawip@gmail.com>
2024-09-26 20:32:15 +0530

    minor

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -19,18 +19,31 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @GetMapping("/v0/{userId}")
+    public List<CatalogItem> getCatalogOlder(@PathVariable("userId") String user){
+        return Collections.singletonList(
+                new CatalogItem(
+                        "Transformers","Test",4)
+        );
+    }
     @Autowired
     private RestTemplate restTemplate;
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
+        //1.get all rated movie ID's
+        //2.for each movie id , call movieInfo service and get details
+        //3.put it all together
+
+        //1.hence this is hardcoded rating part (User has watched and rated these movies -> returned by ratings ms)
         List<Rating> ratings = Arrays.asList(
                 new Rating("1234",9),
                 new Rating("1211",3)
         );
+        //2 . implementation of 2nd & 3rd part
         return ratings.stream().map(rating -> {
-                     Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" +rating.getMovieId(), Movie.class);
-                     return new CatalogItem(movie.getName(), "Hello , this is test",rating.getRating());
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(), "Hello , this is test", rating.getRating());
                 })
                 .collect(Collectors.toList());
 
